2ff4516e11ceba1412ee35c86cf801a1c9c1d1bf
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-01-12 17:25:18 -0700

    finish hashes_2

--- hashes_2.rb
+++ hashes_2.rb
@@ -1,34 +1,34 @@
 # 1- Using the following Hash
 
-states = {
-  oregon: "OR",
-  alabama: "AL",
-  new_jersey: "NJ",
-  colorado: "CO"
-}
+# states = {
+#   oregon: "OR",
+#   alabama: "AL",
+#   new_jersey: "NJ",
+#   colorado: "CO"
+# }
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
-p "Oregon can be abreviated to %{oregon}. Alabama can be abreviated to %{alabama}. New Jersey can be abreviated to %{new_jersey}. Colorado can be abreviated to %{colorado}. " % states
+
+# states.each {|s, a| puts "#{s.capitalize} can be abreviated to #{a}."}
 
 
 
 # ======================================
 # 2- Using the following Hash
 
-birthdays = {
-  "Cindy" => "June 10",
-  "Rigo" => "March 31",
-  "Amos" => "July 21",
-  "Jeff" => "February 12"
-}
+# birthdays = {
+#   "Cindy" => "June 10",
+#   "Rigo" => "March 31",
+#   "Amos" => "July 21",
+#   "Jeff" => "February 12"
+# }
 
-# Use the each method to iterate through it and print out a sentence stating each friends name and birthday
-p ""
-#
-#
+# # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
+
+# birthdays.each {|name, bday| puts"Hello, my name is #{name}, and my birthday is on #{bday}."}
 
 
-# ======================================
-# 3- Using the following Hash
+# # ======================================
+# # 3- Using the following Hash
 
 login_statuses = {
   "Cindy" => true,
@@ -38,6 +38,10 @@ login_statuses = {
 }
 
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
-#
-#
-#
+
+login_statuses.each {|n, lis|
+  if lis == true
+    puts "#{n} is currently logged in."
+  end
+}
+
